348d3bfa4ea907aee02aeb62c234fd0a1530069a
Carlos Narciso <carlos-narciso@live.com>
2018-09-03 11:58:59 +0100

    Challenge five

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,17 +2,19 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// ?
+var qualquer = [28, 83900, 2789, 'string', 19890];
 
 /*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// ?
+function functionArray(arg) {
+    return arg;
+}
 
 /*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// ?
+functionArray(qualquer)[1];
 
 /*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
@@ -20,18 +22,28 @@ segundo, um número. A função deve retornar o valor de um índice do array que
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// ?
+
+function functionAnother(arr, index) {
+    return arr [ index ];
+}
+
 
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// ?
+var arrayFive = [28, null, true, 'string', 19890];
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// ?
+ functionAnother(arrayFive, 0);
+ functionAnother(arrayFive, 1);
+ functionAnother(arrayFive, 2);
+ functionAnother(arrayFive, 3);
+ functionAnother(arrayFive, 4);
+
+
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
