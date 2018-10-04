c066b2fa6063b990b01e64330723f42ced9c2c4d
Carlos Narciso <carlos-narciso@live.com>
2018-10-04 16:54:39 +0100

    Challenge 14 adjusted

--- challenge-14/challenge-14.js
+++ challenge-14/challenge-14.js
@@ -72,8 +72,8 @@
     Mostre o resultado no console.
     */
     console.log( '\nOperation:' );
-    var operation = justMod2Or3.reduce(function (aggregate, current, index) {
-            return justMod2Or3[6] + 1 * current;
+    var operation = justMod2Or3.reduce(function ( aggregate, current ) {
+            return (aggregate + 1) * current;
 
     }, 0);
     console.log(operation);
@@ -83,8 +83,8 @@
     console.
     */
     console.log( '\nOperation 2:' );
-    var operation2 = justMod2Or3.reduceRight(function (aggregate, current, index) {
-            return justMod2Or3[6] + 1 * current;
+    var operation2 = justMod2Or3.reduceRight(function ( aggregate, current ) {
+            return (aggregate + 1) * current;
 
     }, 0);
 
@@ -102,7 +102,7 @@
 
     var myName = name.reduce(function(aggregate, current) {
         return aggregate + 'p' + current;
-    });
+    }, '');
 
     console.log(myName);
     /*
@@ -131,27 +131,32 @@
     */
     console.log( '\nExiste um { number: 2 } em numberObjects?' );
 
-    console.log(numberObjects.indexOf({ number: 2 }) === { number: 2 }
-        ? "Existe um objeto { number: 2 } em numberObjects!"
-        : "Não existe um objeto { number: 2 } em numberObjects :("
-        );
-
+    if ( numberObjects.indexOf( { number: 2 } ) > -1 ){
+        console.log("Existe um objeto { number: 2 } em numberObjects!");
+    }
+    else {
+        console.log("Não existe um objeto { number: 2 } em numberObjects :(");
+    }
     /*
     Fazendo o mesmo do exercício acima, mas começando a buscar do último índice,
     será que obtemos um resultado diferente? Faça a busca a partir do índice 2.
     */
     console.log( '\nE buscando a partir do último índice, o { number: 2 } existe?' );
-    console.log(numberObjects.indexOf({ number: 2 }, 2) === { number: 2 }
-        ? "Existe um objeto { number: 2 } em numberObjects!"
-        : "Não existe um objeto { number: 2 } em numberObjects :("
-        );
+
+    if ( numberObjects.lastIndexOf( { number: 2 }, 2) > -1 ){
+        console.log("Existe um objeto { number: 2 } em numberObjects!");
+    }
+    else {
+        console.log("Não existe um objeto { number: 2 } em numberObjects :(");
+    }
 
     /*
     Verifique se `justMod2Or3` é um array. Se for, mostre-o no console, no
     formato de String.
     */
     console.log( '\njustMod2Or3 é um array? Se for, a representação dele em String é:' );
-    console.log(Array.isArray(justMod2Or3));
-    console.log(justMod2Or3.join());
+    if (Array.isArray(justMod2Or3)) {
+        console.log(justMod2Or3.toString());
+    }
 
 })(); // END IIFE
